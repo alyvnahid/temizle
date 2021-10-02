a52e3502b884b385477f63872a8a87e3c8797e3b
Nakhid <83405153+alyvnahid@users.noreply.github.com>
2021-10-02 23:37:00 +0400

    Add files via upload

---
+++ temizle.sh
@@ -0,0 +1,23 @@
+# Linux sistemlərində ekranı təmizləməyinizə və ip adresinizi öyrənməyinizə yarayacaq kodlama.
+# Author - @nakhidz
+
+# EKRANA YAZDIRMA ###
+printf "
+
+[1] EKRANI TEMIZLE
+[2] IFCONFIG
+"
+# istifadəçi girdisi ##
+read -e -p $'SEÇİM EDİN : ' secim
+
+## ŞƏRTLƏR ##
+
+if [[ $secim == 1]]; then
+	clear
+	exit
+elif [[ $secim2== 2 ]]; then
+ 	 ifconfig
+ 	 exit
+else
+	 printf " XƏTALI SEÇİM "
+fi
